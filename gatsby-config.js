fa1a71a332ed172c4d7fd85f69dcac140f6443b0
Rutul <rutul.patel@ibm.com>
2020-02-24 12:23:16 -0400

    Gatsby + Prismic

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,20 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-prismic-graphql',
+        options: {
+          repositoryName: 'testing-patel', // (REQUIRED, replace with your own)
+          // path: '/preview', // (optional preview path. Default: /preview)
+          // previews: true, // (optional, activated Previews. Default: false)
+        //   pages: [{ // (optional, builds pages dynamically)
+        //   type: 'Article',         // TypeName from prismic
+        //   match: '/article/:uid',  // Pages will be generated under this pattern
+        //   path: '/article',        // Placeholder page for unpublished documents
+        //   component: require.resolve('./src/templates/article.js'),
+        // }],
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,46 @@
 import React from "react"
 import { Link } from "gatsby"
+import { RichText } from "prismic-reactjs";
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { graphql } from "gatsby"
 
-const IndexPage = () => (
+
+export const query = graphql`
+{
+  prismic {
+    allHomepages {
+      totalCount
+      edges {
+        node {
+          title
+          subtitle
+          content
+          heroimage
+        }
+      }
+    }
+  }
+}
+`
+
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    {data.prismic.allHomepages.edges && <div className="not-found">
+      <h1>News</h1>
+      <div className="card_container">
+        <div className="card">
+          <img src={data.prismic.allHomepages.edges[0].node.heroimage.url} alt="Avatar" />
+          <div className="container">
+            <p>
+              <b>{RichText.render(data.prismic.allHomepages.edges[0].node.title)} </b>
+            </p>
+            <p>{RichText.render(data.prismic.allHomepages.edges[0].node.content)}</p>
+          </div>
+        </div>
+      </div>
+    </div>}
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
